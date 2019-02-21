5c620886b3104aea7019642439285d58059a092c
xueyutao <339131001@qq.com>
2019-02-21 11:16:01 +0800

    first commit

---
+++ Assets/Scripts/Demo1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Demo1 : MonoBehaviour
+{
+    public Text m_text;
+
+    void Start()
+    {
+        m_text.text = "哈哈";
+    }
+
+    #region WIN32API
+
+    //[DllImport("user32", CharSet = CharSet.Unicode)]
+    //static extern void GetWindowText();
+    //static extern  int GetWindowText（HWND hWnd，LPTSTR lpString，Int nMaxCount）;
+
+    #endregion
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# WindowsAPI
+# Windows API
+
+-[dnspy](https://blog.csdn.net/Raito__/article/details/82966039 "title")
